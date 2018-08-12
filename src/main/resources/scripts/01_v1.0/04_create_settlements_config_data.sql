8cd1fec6e49e54df0a699150abd15a7cf1b85ad6
vp8791 <neetha.po@gmail.com>
2018-08-12 13:06:32 +0100

    Relase-4.3 tables

--- src/main/resources/scripts/01_v1.0/04_create_settlements_config_data.sql
+++ src/main/resources/scripts/01_v1.0/04_create_settlements_config_data.sql
@@ -3,8 +3,10 @@
 --------------------------------------------------------
 
 Insert into CONFIGURATION_PARAMETERS (KEY,VALUE) values ('COMMIT_SIZE','5');
+Insert into CONFIGURATION_PARAMETERS (KEY,VALUE) values ('SKIP_LIMIT','100');
 Insert into CONFIGURATION_PARAMETERS (KEY,VALUE) values ('LANDING_DIRECTORY','/home/batchconfig/batch/landing');
 Insert into CONFIGURATION_PARAMETERS (KEY,VALUE) values ('SUCCESS_DIRECTORY','/home/batchconfig/batch/success');
 Insert into CONFIGURATION_PARAMETERS (KEY,VALUE) values ('ERROR_DIRECTORY','/home/batchconfig/batch/errors');
 Insert into CONFIGURATION_PARAMETERS (KEY,VALUE) values ('PROCESSING_DIRECTORY','/home/batchconfig/batch/processing');
+Insert into CONFIGURATION_PARAMETERS (KEY,VALUE) values ('SKIPPED_ITEMS_DIRECTORY','/home/batchconfig/batch/skipped');
 commit;
