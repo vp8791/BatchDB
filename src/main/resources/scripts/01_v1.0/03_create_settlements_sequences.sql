8be94f0f6b5afe2e10d1fa84a1150d73a067d1da
vp8791 <neetha.po@gmail.com>
2018-08-12 12:17:45 +0100

    Initila version of DB

---
+++ src/main/resources/scripts/01_v1.0/03_create_settlements_sequences.sql
@@ -0,0 +1,8 @@
+--------------------------------------------------------
+--  File created - Release  4.3    
+--------------------------------------------------------
+
+
+CREATE SEQUENCE JOB_ID_SEQUENCE INCREMENT BY 1 MAXVALUE 9999999999 MINVALUE 1 CACHE 20;
+
+
---
+++ src/main/resources/scripts/01_v1.0/04_create_settlements_config_data.sql
@@ -0,0 +1,10 @@
+--------------------------------------------------------
+--  File created - Release  4.3   
+--------------------------------------------------------
+
+Insert into CONFIGURATION_PARAMETERS (KEY,VALUE) values ('COMMIT_SIZE','5');
+Insert into CONFIGURATION_PARAMETERS (KEY,VALUE) values ('LANDING_DIRECTORY','/home/batchconfig/batch/landing');
+Insert into CONFIGURATION_PARAMETERS (KEY,VALUE) values ('SUCCESS_DIRECTORY','/home/batchconfig/batch/success');
+Insert into CONFIGURATION_PARAMETERS (KEY,VALUE) values ('ERROR_DIRECTORY','/home/batchconfig/batch/errors');
+Insert into CONFIGURATION_PARAMETERS (KEY,VALUE) values ('PROCESSING_DIRECTORY','/home/batchconfig/batch/processing');
+commit;
